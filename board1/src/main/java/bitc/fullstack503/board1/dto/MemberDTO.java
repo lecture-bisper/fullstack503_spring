6ba5d1df5f2a7b7f5843900baeba2e3d77955c9d
lecture-bisper <bisper@outlook.kr>
2025-01-14 16:52:35 +0900

    2025-01-14 : spring - thymeleaf 문법 (선택 변수 사용)

---
+++ board1/src/main/java/bitc/fullstack503/board1/dto/MemberDTO.java
@@ -0,0 +1,24 @@
+package bitc.fullstack503.board1.dto;
+
+import lombok.Data;
+
+@Data
+public class MemberDTO {
+
+  private String id;
+  private String pass;
+  private String name;
+  private String regidate;
+}
+
+
+
+
+
+
+
+
+
+
+
+
