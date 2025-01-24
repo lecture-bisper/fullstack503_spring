132927d4fe9052c2b1244a82af9506e0284ae8f8
lecture-bisper <bisper@outlook.kr>
2025-01-24 16:52:49 +0900

    2025-01-24 : jpa를 사용한 게시판, 더미데이터 자동 추가

---
+++ board4/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into
+    jpa_board (title, contents, create_id, create_date, hit_cnt)
+values
+    ('더미 데이터 제목 01', '더미 데이터 내용 01', 'test1', now(), 0),
+    ('더미 데이터 제목 02', '더미 데이터 내용 02', 'test1', now(), 0),
+    ('더미 데이터 제목 03', '더미 데이터 내용 03', 'test2', now(), 0),
+    ('더미 데이터 제목 04', '더미 데이터 내용 04', 'test2', now(), 0),
+    ('더미 데이터 제목 05', '더미 데이터 내용 05', 'test3', now(), 0);
